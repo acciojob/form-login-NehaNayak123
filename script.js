de6f2ba6ba1d9cbba46ba11f57aaa548d4df08d2
NehaNayak123 <111532433+NehaNayak123@users.noreply.github.com>
2025-02-23 14:53:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const btn=document.getElementById("btn")
 btn.addEventListener("click", getFormvalue);
-
 	let fname=document.querySelector("#fname");
 	let lname=document.querySelector("#lname");
 function getFormvalue(e) {
 	e.preventDefault();
     //Write your code here
 
-	if(fname.value== ""){alert("fill all fields") return}
-	if(lname.value== ""){alert("fill all fields") return}
-
+	if(fname.value== "" || lname.value== ""){
+		alert("fill all fields")
+		return
+	}
+	// if(lname.value== ""){alert("fill all fields") return}
 	fname.value.trim();
 	lname.value.trim();
-	alert(fname.value+lname.value);
+	alert(fname.value+" "+lname.value);
 	fname.value=""
 	lname.value=""
 	
